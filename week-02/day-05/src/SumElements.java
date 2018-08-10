2493990fc05260905f474552549f64004d02ef36
Benedek Jakfalvy <bjakfalvy@gmail.com>
2018-08-10 10:44:40 +0200

    practice arrays

---
+++ week-02/day-05/src/SumElements.java
@@ -0,0 +1,13 @@
+import java.lang.reflect.Array;
+
+public class SumElements {
+    public static void main(String[] args){
+
+// - Create an array variable named `r`
+//   with the following content: `[54, 23, 66, 12]`
+// - Print the sum of the second and the third element
+
+        int[] r = {54, 23, 66, 12};
+        System.out.println(r[1] + r[2]);
+    }
+}
