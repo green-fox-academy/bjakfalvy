f445810fedbc0021831b3df7a5ae7d256f35ad8b
Benedek Jakfalvy <bjakfalvy@gmail.com>
2018-08-14 13:41:00 +0200

    finish task CountLines

---
+++ week-03/day-02/src/CountLines.java
@@ -0,0 +1,24 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class CountLines {
+    public static void main(String[] args){
+        // Write a function that takes a filename as string,
+        // then returns the number of lines the file contains.
+        // It should return zero if it can't open the file, and
+        // should not raise any error.
+
+        System.out.println(numberOfLines("my-file.txt"));
+    }
+    public static int numberOfLines (String fileName){
+        try {
+            Path pathOfFile = Paths.get(fileName);
+            List<String> lines = Files.readAllLines(pathOfFile);
+            return lines.size();
+        } catch (Exception e){
+            return 0;
+        }
+    }
+}
