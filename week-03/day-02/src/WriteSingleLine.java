6723b6250753dd71402c7680f48ca212a433ced5
Benedek Jakfalvy <bjakfalvy@gmail.com>
2018-08-14 13:51:30 +0200

    finish task WriteSingleLine

--- week-03/day-02/src/WriteSingleLine.java
+++ week-03/day-02/src/WriteSingleLine.java
@@ -1,6 +1,8 @@
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WriteSingleLine {
     public static void main(String[] args) {
@@ -9,7 +11,14 @@ public class WriteSingleLine {
         // If the program is unable to write the file,
         // then it should print an error message like: "Unable to write file: my-file.txt"
 
-
-
+        List<String> content = new ArrayList();
+        content.add("Jakfalvy Benedek");
+        try {
+            Path filePath = Paths.get("my-file.txt");
+            Files.write(filePath, content);
+            System.out.println("Tudom olvasni a fajlt.");
+        } catch (Exception e){
+            System.out.println("Unable to write file: my-file.txt");
+        }
     }
 }
