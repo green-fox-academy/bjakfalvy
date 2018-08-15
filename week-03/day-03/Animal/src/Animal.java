5234d90c8878e871d400e7306600cd94182dd997
Benedek Jakfalvy <bjakfalvy@gmail.com>
2018-08-15 21:19:24 +0200

    finish task Animal

--- week-03/day-03/Animal/src/Animal.java
+++ week-03/day-03/Animal/src/Animal.java
@@ -1,3 +1,20 @@
 public class Animal {
+        int hunger;
+        int thirst;
 
+        void creatingAnimal(int hunger, int thirst){
+            this.hunger = 50;
+            this.thirst = 50;
+        }
+
+        void eat(){
+            this.hunger --;
+        }
+        void drink(){
+            this.thirst --;
+        }
+        void play(){
+            this.hunger ++;
+            this.thirst ++;
+        }
 }
