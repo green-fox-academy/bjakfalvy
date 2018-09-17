5d5610386f55b43ef52df58a7643643321760d2f
Benedek Jakfalvy <bjakfalvy@gmail.com>
2018-09-17 14:02:15 +0200

    REST Greet counter app

--- week-07/helloworld/src/main/java/com/greenfoxacademy/helloworld/Greeting.java
+++ week-07/helloworld/src/main/java/com/greenfoxacademy/helloworld/Greeting.java
@@ -1,8 +1,11 @@
 package com.greenfoxacademy.helloworld;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class Greeting {
     private long id;
     private String content;
+    static AtomicLong currentID = new AtomicLong(0);
 
     public Greeting(long id, String content) {
         this.id = id;
@@ -23,5 +26,13 @@ public class Greeting {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public static AtomicLong getCurrentID() {
+        return currentID;
+    }
+
+    public void setCurrentID(AtomicLong currentID) {
+        Greeting.currentID = currentID;
     }
 }
--- week-07/helloworld/src/main/java/com/greenfoxacademy/helloworld/controllers/HelloRESTController.java
+++ week-07/helloworld/src/main/java/com/greenfoxacademy/helloworld/controllers/HelloRESTController.java
@@ -10,6 +10,6 @@ public class HelloRESTController {
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam("name") String name){
-       return new Greeting(1, "Hello, " + name);
+       return new Greeting(Greeting.getCurrentID().incrementAndGet(), "Hello, " + name);
     }
 }
