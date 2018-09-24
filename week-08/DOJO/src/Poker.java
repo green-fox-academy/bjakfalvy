7de88337532aaef78e52a7b0d4d0cff1c88579b2
Benedek Jakfalvy <bjakfalvy@gmail.com>
2018-09-24 16:31:06 +0200

    DOJO Poker finished

--- week-08/DOJO/src/Poker.java
+++ week-08/DOJO/src/Poker.java
@@ -24,4 +24,30 @@ public class Poker {
         }
         return "White wins!";
     }
+
+    public boolean differingValues(ArrayList<Card> list1){
+        for (int i = 0; i < list1.size()-1; i++) {
+            for (int j = i + 1; j < list1.size(); j++) {
+                if (list1.get(i).getValue() == list1.get(j).getValue()){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public String isHighCard(ArrayList<Card> list){
+        if (differingValues(list) && isFlush(list) == "") {
+            return "Black wins!";
+        }
+        return "";
+    }
+
+    public String isTie(ArrayList<Card> list1, ArrayList<Card> list2){
+        if (list1 == list2){
+            return "";
+        }
+        return "Tie";
+
+    }
 }
--- week-08/DOJO/test/PokerTest.java
+++ week-08/DOJO/test/PokerTest.java
@@ -66,4 +66,64 @@ public class PokerTest {
 
         assertEquals("White wins!", pokerGame.isFlush(white));
     }
+
+    @Test
+    public void blackHighCard() {
+        ArrayList<Card> black = new ArrayList<Card>(5);
+        Card card1 = new Card("H", 2);
+        Card card2 = new Card("S", 3);
+        Card card3 = new Card("C", 5);
+        Card card4 = new Card("D", 9);
+        Card card5 = new Card("H", 13);
+        black.add(card1);
+        black.add(card2);
+        black.add(card3);
+        black.add(card4);
+        black.add(card5);
+
+        ArrayList<Card> white = new ArrayList<>(5);
+        Card card6 = new Card("S", 2);
+        Card card7 = new Card("D", 8);
+        Card card8 = new Card("S", 14);
+        Card card9 = new Card("H", 8);
+        Card card10 = new Card("S", 13);
+        white.add(card6);
+        white.add(card7);
+        white.add(card8);
+        white.add(card9);
+        white.add(card10);
+
+        assertEquals("Black wins!",pokerGame.isHighCard(black));
+    }
+
+    @Test
+    public void isTie() {
+        ArrayList<Card> black = new ArrayList<Card>(5);
+        Card card1 = new Card("H", 2);
+        Card card2 = new Card("D", 3);
+        Card card3 = new Card("S", 5);
+        Card card4 = new Card("C", 9);
+        Card card5 = new Card("D", 13);
+        black.add(card1);
+        black.add(card2);
+        black.add(card3);
+        black.add(card4);
+        black.add(card5);
+
+        ArrayList<Card> white = new ArrayList<>(5);
+        Card card6 = new Card("S", 2);
+        Card card7 = new Card("D", 3);
+        Card card8 = new Card("S", 5);
+        Card card9 = new Card("H", 9);
+        Card card10 = new Card("S", 13);
+        white.add(card6);
+        white.add(card7);
+        white.add(card8);
+        white.add(card9);
+        white.add(card10);
+
+        assertEquals("Tie",pokerGame.isTie(black,white));
+    }
+
+
 }
