0d2b30d1e2a5c2070f797d3f4f77eeba3ee2b1e6
Benedek Jakfalvy <bjakfalvy@gmail.com>
2018-10-02 14:16:09 +0200

    integration test is done

---
+++ week-08/Reddit/src/test/java/com/greenfoxacademy/reddit/RedditApplicationTests.java
@@ -0,0 +1,33 @@
+package com.greenfoxacademy.reddit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class RedditApplicationTests {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void contextLoads() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void submitPage() throws Exception {
+        mockMvc.perform(get("/submit"))
+                .andExpect(status().isOk());
+    }
+}
