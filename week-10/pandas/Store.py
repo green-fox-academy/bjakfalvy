07ade0dbf1159cb74a08cda85e938f0ecb91bdbf
Benedek Jakfalvy <bjakfalvy@gmail.com>
2018-10-12 12:49:59 +0200

    Reading in googleplaystore.csv

---
+++ week-10/pandas/Store.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+filename = 'googleplaystore.csv'
+df = pd.read_csv(filename)
+
+
+print("\n<<<<<DESCRIBE>>>>>\n")
+print(df.describe())
+print("\n<<<<<DATA TYPE>>>>>\n")
+print(df.dtypes)
+print("\n<<<<<INDEX>>>>>\n")
+print(df.index)
+print("\n<<<<<COLUMNS>>>>>\n")
+print(df.columns)
+print("\n<<<<<VALUES>>>>>\n")
+print(df.values)
+
+print("<<<<<ITERATING>>>>>")
+for index, row in df.iterrows():
+    print(index,row["App"], row["Rating"])
