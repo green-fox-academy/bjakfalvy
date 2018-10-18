e4a6c4b1084e8d8873416ecb3739b5aa1669e275
Benedek Jakfalvy <bjakfalvy@gmail.com>
2018-10-18 16:14:47 +0200

    changed input field to 'input' from 'raw_input'

--- week-11/wiki/Wikipedia.py
+++ week-11/wiki/Wikipedia.py
@@ -1,6 +1,5 @@
 import wikipedia as wiki
-from click._compat import raw_input
 
-topic = raw_input('Enter a topic: ')
+topic = input('Enter a topic: ')
 wiki_shark = wiki.summary(topic)
 print(wiki_shark)
